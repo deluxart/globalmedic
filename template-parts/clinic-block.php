cb4238e2bf7c4d8a82cf8af319d668e8945b69b6
deluxart <deluxart3@gmail.com>
2020-09-20 18:31:06 +0300

    change slider

--- template-parts/clinic-block.php
+++ template-parts/clinic-block.php
@@ -9,134 +9,101 @@
  * @since Twenty Fifteen 1.0
  */
 ?>
-<!-- Блок одной клиники с фото -->
-
-
-
-
-
-
-
-
-
-
-
-<section class="one-clinic blue">
+<section class="one-clinic">
     <div class="container">
         <div class="content">
             <div class="text">
                 <h3><?php the_title(); ?></h3>
 
-
-<?php if ( have_rows( 'clinick_block' ) ) : ?>
-    <?php while ( have_rows( 'clinick_block' ) ) : the_row(); ?>
-        <p><?php the_sub_field( 'short_description' ); ?></p>
-    <?php endwhile; ?>
-<?php endif; ?>
-
+                <?php if ( have_rows( 'clinick_block' ) ) : ?>
+                    <?php while ( have_rows( 'clinick_block' ) ) : the_row(); ?>
+                        <p><?php the_sub_field( 'short_description' ); ?></p>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+                <?php if ( have_rows( 'tags_for_clinic' ) ) : ?>
                 <ul class="clinic-tags">
-
-<?php if ( have_rows( 'tags_for_clinic' ) ) : ?>
-	<?php while ( have_rows( 'tags_for_clinic' ) ) : the_row(); ?>
-		<li><?php the_sub_field( 'tag' ); ?></li>
-	<?php endwhile; ?>
-<?php endif; ?>
-
-                <!-- < ?php $rows_tag = get_field('tags_for_clinic');
-                foreach ( $rows_tag as $row_tag ) : ?>
-                    <li>< ?php $row_tag['tag']; ?></li>
-                < ?php endforeach;?> -->
+                <?php while ( have_rows( 'tags_for_clinic' ) ) : the_row(); ?>
+                    <li><?php the_sub_field( 'tag' ); ?></li>
+                <?php endwhile; ?>
+                
                 </ul>
+                <?php endif; ?>
 
 
             </div>
 
             <div class="photos">
 
-
-
-<?php if ( have_rows( 'photo_for_gallery' ) ) : ?>
-<div class="medic-slider slider-pro">
-    <div class="sp-slides">
-    <?php while ( have_rows( 'photo_for_gallery' ) ) : the_row(); ?>
-        
-        <?php if ( get_sub_field( 'foto' ) ) { ?>
-        <div class="sp-slide"><img class="sp-image" src="<?php echo get_template_directory_uri(); ?>/img/blank.gif" data-src="<?php the_sub_field( 'foto' ); ?>"/></div>
-        <?php } ?>
-        
-    <?php endwhile; ?>
-</div>
-<div class="sp-thumbnails">
-    <?php while ( have_rows( 'photo_for_gallery' ) ) : the_row(); ?>
-        <?php if ( get_sub_field( 'foto' ) ) { ?>
-            <img class="sp-thumbnail" src="<?php the_sub_field( 'foto' ); ?>"/>
-        <?php } ?>
-
-    <?php endwhile; ?>
-</div>
-</div>
-<?php else : ?>
-    <?php // no rows found ?>
-<?php endif; ?>    
-
-    
-        
-
+            <?php if ( have_rows( 'photo_for_gallery' ) ) : ?>
+            <div class="med-slider">
+                <div class="swiper-container gallery-top">
+                    <div class="swiper-wrapper">
+                    <?php while ( have_rows( 'photo_for_gallery' ) ) : the_row(); ?>
+                        <?php if ( get_sub_field( 'foto' ) ) { ?>
+                        <div class="swiper-slide"><img src="<?php the_sub_field( 'foto' ); ?>" alt=""></div>
+                        <?php } ?>
+                    <?php endwhile; ?>
+                    </div>
+                    <!-- Add Arrows -->
+                    <div class="swiper-button-next swiper-button-white"></div>
+                    <div class="swiper-button-prev swiper-button-white"></div>
+                </div>
+                <div class="swiper-container gallery-thumbs">
+                    <div class="swiper-wrapper">
+                    <?php while ( have_rows( 'photo_for_gallery' ) ) : the_row(); ?>
+                        <?php if ( get_sub_field( 'foto' ) ) { ?>
+                        <div class="swiper-slide"><img src="<?php the_sub_field( 'foto' ); ?>" alt=""></div>
+                        <?php } ?>
+                    <?php endwhile; ?>
+                    </div>
+                </div>
+            </div>
+            <?php else : ?>
+                <?php // no rows found ?>
+            <?php endif; ?>    
             </div>
 
 
             <div>
+            <?php if ( have_rows( 'clinick_block' ) ) : ?>
+                <?php while ( have_rows( 'clinick_block' ) ) : the_row(); ?>
 
-
-
-
-<?php if ( have_rows( 'clinick_block' ) ) : ?>
-    <?php while ( have_rows( 'clinick_block' ) ) : the_row(); ?>
-
-                <div class="counters">
-        <?php if ( have_rows( 'count_1' ) ) : ?>
-            <?php while ( have_rows( 'count_1' ) ) : the_row(); ?>
-                    <div>
-                        <div class="num"><?php the_sub_field( 'count_num' ); ?></div>
-                        <p><?php the_sub_field( 'opisanie' ); ?></p>
+                            <div class="counters">
+                    <?php if ( have_rows( 'count_1' ) ) : ?>
+                        <?php while ( have_rows( 'count_1' ) ) : the_row(); ?>
+                                <div>
+                                    <div class="num"><?php the_sub_field( 'count_num' ); ?></div>
+                                    <p><?php the_sub_field( 'opisanie' ); ?></p>
+                                </div>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+                    <?php if ( have_rows( 'count_2' ) ) : ?>
+                        <?php while ( have_rows( 'count_2' ) ) : the_row(); ?>
+                                <div>
+                                    <div class="num"><?php the_sub_field( 'count_num' ); ?></div>
+                                    <p><?php the_sub_field( 'opisanie' ); ?></p>
+                                </div>
+                            
+                            
+                        <?php endwhile; ?>
+                    <?php endif; ?>
                     </div>
-            <?php endwhile; ?>
-        <?php endif; ?>
-        <?php if ( have_rows( 'count_2' ) ) : ?>
-            <?php while ( have_rows( 'count_2' ) ) : the_row(); ?>
-                    <div>
-                        <div class="num"><?php the_sub_field( 'count_num' ); ?></div>
-                        <p><?php the_sub_field( 'opisanie' ); ?></p>
-                    </div>
-                
-                
-            <?php endwhile; ?>
-        <?php endif; ?>
-        </div>
-    <?php endwhile; ?>
-<?php endif; ?>
-     
-
+                <?php endwhile; ?>
+            <?php endif; ?>
             </div>
 
             <div class="btns">
                 <ul class="clinic-links">
-
-<?php if ( have_rows( 'clinick_block' ) ) : ?>
-    <?php while ( have_rows( 'clinick_block' ) ) : the_row(); ?>
-                    <li><a href="<?php the_sub_field( 'link_clinic_page' ); ?>" class="btn">О клиникие</a></li>
-                    <li><a class="popmake-obratnyj-zvonok green-btn btn">Уточнить цены</a></li>
-    <?php endwhile; ?>
-<?php endif; ?>
+                <?php if ( have_rows( 'clinick_block' ) ) : ?>
+                    <?php while ( have_rows( 'clinick_block' ) ) : the_row(); ?>
+                        <li><a href="<?php the_sub_field( 'link_clinic_page' ); ?>" class="btn">О клиникие</a></li>
+                        <li><a class="popmake-obratnyj-zvonok green-btn btn">Уточнить цены</a></li>
+                    <?php endwhile; ?>
+                <?php endif; ?>
                 </ul>
             </div>
-
-
-
-
         </div>
     </div>
 </section>
-<!-- Конец - Блок одной клиники с фото -->
         
     
